9dd92a979c721608451eb07b76fc24a73fb00c09
Marc <498834+cawoodm@users.noreply.github.com>
2018-08-07 15:35:33 +0200

    screenshot

--- publish.bat
+++ publish.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 ::MD ..\..\..\cawoodm.github.io\rockdodger
-git add . && git commit -a -m %1
+git add . && git commit -a -m %1 && git push
 PUSHD ..\..\..\cawoodm.github.io\rockdodger
 ::DEL *.js
 XCOPY /y /e /d ..\..\JavaScript\games\rockdodger . && git add . && git commit -a -m %1 && git push
